bd218d334c3db92460a36e85853d25f9fb259f36
JulianBenitez <julian.benitezg@outlook.com>
2023-09-10 13:05:19 -0500

    feat(gocsvrepo/main.py): save go repos info on csv

--- GoCSVRepo/const/constants.py
+++ GoCSVRepo/const/constants.py
@@ -1,2 +1,3 @@
 GITHUB_ACCESS_TOKEN = 'GITHUB_ACCESS_TOKEN'
-GO_MOD_REGEX = r'go\s(\d+\.\d+(\.\d+)?)'
+ATTRIBUTES = ['full_name', 'html_url', 'stargazers_count', 'forks_count', 'collaborators', 'open_issues_count',
+              'description', 'archived', 'created_at', 'updated_at', 'pushed_at']
--- GoCSVRepo/main.py
+++ GoCSVRepo/main.py
@@ -1,12 +1,12 @@
+import csv
 import os
-import re
 
 from github import Auth
 from github import Github
 from github import PaginatedList
 from github import Repository
 
-from const.constants import GITHUB_ACCESS_TOKEN, GO_MOD_REGEX
+from const.constants import GITHUB_ACCESS_TOKEN, ATTRIBUTES
 
 
 def search_repositories_by_language(
@@ -21,32 +21,51 @@ def search_repositories_by_language(
     :param language: the language to search for
     :param sort: the sort field (stars, forks, updated) - default: stars
     :param order: the sort order (asc, desc) - default: desc
+    :return: a paginated list of repositories
+    """
+    return g.search_repositories(query=f"language:{language}", sort=sort, order=order)
+
+
+def get_metadata_from_repository(
+        repo: Repository.Repository,
+) -> dict:
+    """
+    Get metadata from a repository.
+    :param repo: a GitHub repository
+    :return: a dictionary with the repository metadata
+    """
+    metadata = {}
+    metadata['full_name'] = repo.full_name
+    metadata['html_url'] = repo.html_url
+    metadata['stargazers_count'] = repo.stargazers_count
+    metadata['forks_count'] = repo.forks_count
+    metadata['collaborators'] = repo.get_contributors().totalCount
+    metadata['open_issues_count'] = repo.open_issues_count
+    metadata['description'] = repo.description
+    metadata['archived'] = repo.archived
+    metadata['created_at'] = repo.created_at
+    metadata['updated_at'] = repo.updated_at
+    metadata['pushed_at'] = repo.pushed_at
+    return metadata
+
+
+def save_repositories_csv(
+        repositories: PaginatedList.PaginatedList[Repository.Repository],
+        total_repositories: int = 10,
+        file_name: str = 'repositories.csv'):
+    """
+    Save repositories to a CSV file.
+    :param repositories: the repositories paginated list from GitHub
+    :param total_repositories: the total number of repositories to save
+    :param file_name: file name to save the CSV
     :return: nothing
     """
-    repositories = g.search_repositories(query=f"language: {language}", sort=sort, order=order)
-    return repositories
-
-
-def get_go_version(
-        repositories: PaginatedList.PaginatedList[Repository.Repository],
-        page_size: int = 10
-) -> [[Repository.Repository, str]]:
-    """
-    Get the go version from the go.mod file.
-    :param repositories: the repositories to search
-    :param page_size: the number of repositories to search - default: 10
-    :return: a list of tuples containing the repository and the go version
-    """
-    repos = []
-    for repo in repositories[:page_size]:
-        try:
-            go_mod_content = repo.get_contents('go.mod').decoded_content.decode()
-            match = re.search(GO_MOD_REGEX, go_mod_content)
-            if match:
-                repos.append((repo, match.group(1)))
-        except Exception:
-            print("Could not find go.mod file in repository: ", repo.full_name)
-    return repos
+    with open(f'output/{file_name}', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=ATTRIBUTES)
+        writer.writeheader()
+        for repo in repositories[:total_repositories]:
+            repo_metadata = get_metadata_from_repository(repo)
+            writer.writerow(repo_metadata)
 
 
 if __name__ == '__main__':
@@ -55,5 +74,4 @@ if __name__ == '__main__':
     auth = Auth.Token(os.environ[GITHUB_ACCESS_TOKEN])
     g = Github(auth=auth)
     go_repos = search_repositories_by_language(g, 'go')
-    versions = get_go_version(go_repos)
-    print(versions)
+    save_repositories_csv(go_repos, 100)
