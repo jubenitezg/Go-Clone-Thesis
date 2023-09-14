378372d579adb4a62f2985acb3378c1210549a68
JulianBenitez <julian.benitezg99@gmail.com>
2023-09-13 21:24:54 -0500

    wip: working first ast path

--- GoExtractor/go.mod
+++ GoExtractor/go.mod
@@ -2,4 +2,3 @@ module go-extractor
 
 go 1.21.1
 
-require golang.org/x/tools v0.13.0
--- GoExtractor/main.go
+++ GoExtractor/main.go
@@ -5,8 +5,12 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
-	"golang.org/x/tools/go/ast/inspector"
+	"slices"
+	"strings"
 )
+
+const Up = "↑"
+const Down = "↓"
 
 func parseGoFile(filename string) (*ast.File, *token.FileSet, error) {
 	fs := token.NewFileSet()
@@ -17,72 +21,96 @@ func parseGoFile(filename string) (*ast.File, *token.FileSet, error) {
 	return node, fs, nil
 }
 
-func extractFunctions(parsedAST *ast.File, fset *token.FileSet) []*ast.FuncDecl {
+func extractFunctions(parsedAST *ast.File) []*ast.FuncDecl {
 	var funcDecls []*ast.FuncDecl
-
 	ast.Inspect(parsedAST, func(node ast.Node) bool {
 		if funcDecl, ok := node.(*ast.FuncDecl); ok {
 			funcDecls = append(funcDecls, funcDecl)
 		}
-		return true // Continue the traversal
+		return true
 	})
-
-	for _, funcDecl := range funcDecls {
-		fmt.Println("Function: ", funcDecl.Name)
-		fmt.Println("Start: ", fset.Position(funcDecl.Pos()))
-		fmt.Println("End: ", fset.Position(funcDecl.End()))
-	}
 
 	return funcDecls
 }
 
-func discoverNodeParentsWithStack(parsedAST []*ast.File, fset *token.FileSet) {
-	insp := inspector.New(parsedAST)
-	insp.WithStack(nil, func(n ast.Node, push bool, stack []ast.Node) bool {
-		if bexpr, ok := n.(*ast.BinaryExpr); push && ok && bexpr.Op == token.ADD {
-			for i := len(stack) - 2; i >= 0; i-- {
-				if _, ok := stack[i].(*ast.BinaryExpr); ok {
-					fmt.Printf("found BinaryExpr(+) as a child of another binary expr: %v\n",
-						fset.Position(n.Pos()))
-					break
-				}
-			}
+// getTreeStack will return the stack of nodes from the root to the node provided
+// it needs the function declaration to be able to get the stack
+func getTreeStack(funcDecl *ast.FuncDecl, node *ast.Node) []ast.Node {
+	var tmpStack []ast.Node
+	var stack []ast.Node
+	ast.Inspect(funcDecl, func(n ast.Node) bool {
+		if n == *node {
+			stack = make([]ast.Node, len(tmpStack))
+			copy(stack, tmpStack)
+			return false
+		}
+		if n == nil {
+			tmpStack = tmpStack[:len(tmpStack)-1]
+		} else {
+			tmpStack = append(tmpStack, n)
 		}
 		return true
 	})
+	stack = append(stack, *node)
+	slices.Reverse(stack)
+
+	return stack
 }
 
-func extractLeavesFromFunc(funcDecl *ast.FuncDecl, fset *token.FileSet) []ast.Node {
+func extractLeavesFromFunc(funcDecl *ast.FuncDecl) []ast.Node {
 	var leafNodes []ast.Node
-
 	ast.Inspect(funcDecl, func(node ast.Node) bool {
 		switch node.(type) {
 		case *ast.Ident, *ast.BasicLit, *ast.CompositeLit:
 			leafNodes = append(leafNodes, node)
 		}
-		return true // Continue the traversal
+		return true
 	})
 
-	for _, leafNode := range leafNodes {
-		fmt.Printf("Leaf Node Type: %T\n", leafNode)
-		fmt.Println("Leaf Node: ", leafNode.Pos(), leafNode.End())
-		fmt.Println(fset.Position(leafNode.Pos()).Line)
-	}
 	return leafNodes
 }
 
-//func generatePath()
+func generatePath(funcDecl *ast.FuncDecl, source *ast.Node, target *ast.Node) string {
+	sourceTreeStack := getTreeStack(funcDecl, source)
+	targetTreeStack := getTreeStack(funcDecl, target)
+	var pathBuilder strings.Builder
+	commonPrefix := 0
+	currentSourceAncestor := len(sourceTreeStack) - 1
+	currentTargetAncestor := len(targetTreeStack) - 1
+	for currentSourceAncestor >= 0 && currentTargetAncestor >= 0 &&
+		sourceTreeStack[currentSourceAncestor] == targetTreeStack[currentTargetAncestor] {
+		commonPrefix++
+		currentSourceAncestor--
+		currentTargetAncestor--
+	}
+
+	for i := 0; i < len(sourceTreeStack)-commonPrefix; i++ {
+		current := sourceTreeStack[i]
+		pathBuilder.WriteString(fmt.Sprintf("%T%s", current, Up))
+	}
+
+	common := sourceTreeStack[len(sourceTreeStack)-commonPrefix]
+	pathBuilder.WriteString(fmt.Sprintf("%T", common))
+
+	for i := len(targetTreeStack) - commonPrefix - 1; i >= 0; i-- {
+		current := targetTreeStack[i]
+		pathBuilder.WriteString(fmt.Sprintf("%s%T", Down, current))
+	}
+	return pathBuilder.String()
+}
 
 func main() {
 	filename := "test.go"
-	parsedAST, fset, err := parseGoFile(filename)
+	parsedAST, _, err := parseGoFile(filename)
 	if err != nil {
 		fmt.Println("Error parsing Go file:", err)
 		return
 	}
 
 	//fmt.Println(parsedAST)
-	discoverNodeParentsWithStack([]*ast.File{parsedAST}, fset)
+	functions := extractFunctions(parsedAST)
+	leaves := extractLeavesFromFunc(functions[0])
+	fmt.Println(generatePath(functions[0], &leaves[7], &leaves[10]))
 	//for _, function := range extractFunctions(parsedAST, fset) {
 	//	//fmt.Println("Function: ", function.Name.Name)
 	//	leaves := extractLeavesFromFunc(function, fset)
