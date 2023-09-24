49a6e6d8ca7d0d69d459745da0ec97869e5d2883
JulianBenitez <julian.benitezg99@gmail.com>
2023-09-24 00:27:43 -0500

    wip: add flags for execution

---
+++ GoExtractor/README.md
@@ -0,0 +1,24 @@
+# GoExtractor
+A go AST path extractor
+
+## Pre-requisites
+
+* Go 1.21 or higher 
+* [Poetry](https://python-poetry.org/docs/#installation)
+
+
+## Install
+From the root of the project run:
+
+```bash
+go build -o gextr
+```
+
+## Run
+
+```bash
+./gextr --file <path_to_file> [--hash]
+```
+
+## Author
+* Julián Benítez Gutiérrez - [julian.benitez@mail.escuelaing.edu.co](mailto:julian.benitez@mail.escuelaing.edu.co)
--- GoExtractor/main.go
+++ GoExtractor/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"go-extractor/extractor"
+	"os"
 )
 
+var (
+	hash *bool
+	file *string
+)
+
+func init() {
+	hash = flag.Bool("hash", false, "hash path (default: false)")
+	file = flag.String("file", "", "file to parse (required)")
+	flag.Usage = func() {
+		_, err := fmt.Fprintf(os.Stderr, "Usage of gextr\n")
+		if err != nil {
+			panic(err)
+		}
+		flag.PrintDefaults()
+	}
+}
+
 func main() {
-	filename := "extractor/test_assets/test.go"
-	ex, err := extractor.NewExtractor(filename, false)
+	flag.Parse()
+	ex, err := extractor.NewExtractor(*file, *hash)
 	if err != nil {
 		fmt.Println("Error creating extractor:", err)
 		return
