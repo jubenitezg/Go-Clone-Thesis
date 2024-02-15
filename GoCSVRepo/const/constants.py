185d0a598d844cfb79f5c0d148a25fec452be74e
JulianBenitez <julian.benitezg@outlook.com>
2024-02-14 22:21:54 -0500

    refactor(gocsv): remove miner project

--- GoCSVRepo/const/constants.py
+++
@@ -1,6 +0,0 @@
-GITHUB_ACCESS_TOKEN = 'GITHUB_ACCESS_TOKEN'
-ATTRIBUTES = ['full_name', 'html_url', 'stargazers_count', 'forks_count', 'collaborators', 'open_issues_count',
-              'description', 'archived', 'created_at', 'updated_at', 'pushed_at']
-
-REPOS_PATH = 'collector/output'
-STATE_PATH = 'collector/output/state.json'
