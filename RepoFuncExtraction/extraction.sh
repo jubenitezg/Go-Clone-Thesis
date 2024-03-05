8122420816c4dc7f66db7a957784a2e9ade5e186
Julian Benitez Gutierrez <julian.benitezg99@gmail.com>
2024-03-04 20:23:10 -0500

    Repositories Function Extraction (#9)
    * wip: clone, extract and save functions from miner
    
    * wip: single-line output for extraction
    
    * feat(extractor): script for cloning and extracting functions
    
    * style(repoextraction): rename directory

---
+++ RepoFuncExtraction/extraction.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+source helpers/function-extraction.sh
+
+# Enable debug mode
+# set -x
+
+cwd=$(pwd)
+EXTRACT_EXE="gfunc"
+TMP_DIR="/tmp/temp-repo"
+
+print_usage() {
+  printf "Usage: -o for output directory (needed)\n"
+}
+
+setup() {
+    build_function_extractor "${EXTRACT_EXE}"
+    rm -rf "${TMP_DIR}"
+}
+
+main() {
+    setup
+    pushd "${cwd}/../GitHubMiner/output"
+        i=0
+        jq -r '.[:10] | .[] | "\(.name),\(.owner),\(.url)"' "go-repositories.json" | while IFS= read -r line; do
+            IFS=',' read -r name owner url <<< "$line"
+            echo "Cloning repository: ${owner}/${name}"
+            git clone "${url}" "${TMP_DIR}"
+            echo "Extracting functions from repository: ${owner}/${name}"
+            "${cwd}/${EXTRACT_EXE}" --project-path "${TMP_DIR}" --single-line > "${OUTPUT_DIR}/${owner}-${name}.json"
+            rm -rf "${TMP_DIR}"
+            echo "Processed repository: ${i}"
+            echo "Processed repository: ${i}" > "${cwd}/current.log"
+            i=$((i+1))
+        done
+    popd
+}
+
+while getopts 'o:' flag; do
+  case "${flag}" in
+    o) OUTPUT_DIR="${OPTARG}" ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+if [ -z "${OUTPUT_DIR}" ]; then
+    print_usage
+    exit 1
+fi
+
+main
